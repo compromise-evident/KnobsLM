e097797328c2fed8e58eb40701b4fee77e2c46b2
Nikolay Valentinovich Repnitskiy <nikolay_valentinovich_repnitskiy@protonmail.com>
2023-03-09 03:24:29 -0700

    Add files via upload

--- KnobsLM.cpp
+++ KnobsLM.cpp
@@ -2,7 +2,7 @@
 /// Nikolay Valentinovich Repnitskiy - License: WTFPLv2+ (wtfpl.net)
 
 
-/*  Version 1.0.0  Uses 1 CPU & up to 500MB RAM.
+/*  Version 1.0.0  Uses 1 CPU core & up to 500MB RAM.
  ______________________________________________________________________________
 /                                                                              \
 |                   Create text file Training_data and fill                    |
